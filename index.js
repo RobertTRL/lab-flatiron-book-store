9a4a1b5b38f9fce18826f9832b7f6e45c3268dff
Robert Toroitich <robertojnr2802@gmail.com>
2026-04-02 16:06:54 +0300

    Wrote code

--- index.js
+++ index.js
@@ -44,4 +44,24 @@ const bookStore = {
 }
 
 // Write your code here!
+const bookStoreTitle = document.getElementById("header")
+bookStoreTitle.textContent = bookStore.name
+
+bookStore.books.forEach(book => {
+    const bookContainer = document.createElement("li")
+    const bookTitle = document.createElement("h3")
+    bookTitle.textContent = book.title
+    const bookAuthor = document.createElement("p")
+    bookAuthor.textContent = book.author
+    const bookImage =document.createElement("img")
+    bookImage.src = book.imageUrl
+    const booklist = document.getElementById("book-list")
+    bookContainer.append(bookTitle, bookAuthor, bookImage)
+    booklist.append(bookContainer)
+
+})
+
+const elementToDelete = document.getElementById("delete-this")
+elementToDelete.remove()
+    
 
